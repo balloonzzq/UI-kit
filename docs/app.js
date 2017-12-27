833e67ee992af7f613aa0a45f10830e25e48573c
庄再茜 <zqzhuang888@xkeshi.com>
2017-12-27 15:38:35 +0800

    change listen

--- docs/app.js
+++ docs/app.js
@@ -5,5 +5,5 @@ const data = fs.readFileSync('./index.html')
 http.createServer((req, res) => {
 res.writeHead(200, {'Content-Type': 'text/html'})
 res.end(data)
-}).listen(80)
-console.log('server runing on: http://39.108.217.94:80 ')
+}).listen(8082)
+console.log('server runing on: http://39.108.217.94:8082 ')
