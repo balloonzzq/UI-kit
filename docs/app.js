36be9c958e321a53c80ff089d4e60cf99fbb87a7
庄再茜 <zqzhuang888@xkeshi.com>
2017-12-27 16:08:23 +0800

    add server

--- docs/app.js
+++
@@ -1,9 +0,0 @@
-
-const http = require('http')
-const  fs = require("fs")
-const data = fs.readFileSync('./index.html')
-http.createServer((req, res) => {
-res.writeHead(200, {'Content-Type': 'text/html'})
-res.end(data)
-}).listen(8082)
-console.log('server runing on: http://39.108.217.94:8082 ')
